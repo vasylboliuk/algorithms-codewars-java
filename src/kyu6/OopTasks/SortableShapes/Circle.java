d85759bfea8a778a59818164faa5b63538ba5370
vasylboliuk <vbboliuk@gmail.com>
2017-05-07 12:59:59 +0300

    Add new Solution 6kyu: Sortable Shapes

---
+++ src/kyu6/OopTasks/SortableShapes/Circle.java
@@ -0,0 +1,18 @@
+package kyu6.OopTasks.SortableShapes;
+
+/**
+ * Created by Dell on 5/6/2017.
+ */
+public class Circle extends Shape{
+
+    double radius;
+
+    public Circle(double radius){
+        this.radius = radius;
+    }
+
+    @Override
+    public double area(){
+        return this.radius*this.radius*Math.PI;
+    }
+}
---
+++ src/kyu6/OopTasks/SortableShapes/CustomShape.java
@@ -0,0 +1,18 @@
+package kyu6.OopTasks.SortableShapes;
+
+/**
+ * Created by Dell on 5/6/2017.
+ */
+public class CustomShape extends Shape{
+
+    double area;
+
+    public CustomShape(double area){
+        this.area = area;
+    }
+    
+    @Override
+    public double area(){
+        return this.area;
+    }
+}
